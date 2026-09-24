62bebc4a16f5b40780aa7e26ea95fa94dc38622e
kebibg <46591284+kebibg@users.noreply.github.com>
2026-09-24 13:20:07 +0300

    Restore Use Sensor switch state on startup

--- components/fujitsu-halcyon/esphome-custom-switch.h
+++ components/fujitsu-halcyon/esphome-custom-switch.h
@@ -9,12 +9,23 @@ namespace esphome::custom {
 
 class CustomSwitch : public Component, public switch_::Switch {
     public:
-        CustomSwitch(std::function<bool(bool)> func) : func(func) {};
+        CustomSwitch(std::function<bool(bool)> func, bool restore_on_setup = false) : func(func), restore_on_setup_(restore_on_setup) {};
+
+        void setup() override {
+            if (!this->restore_on_setup_)
+                return;
+
+            auto initial_state = this->get_initial_state_with_restore_mode();
+            if (initial_state.has_value())
+                this->control(initial_state.value());
+        }
+
         void write_state(bool state) override { this->publish_state(this->func(state) ? state : this->state); };
 
     private:
         CustomSwitch() {};
         std::function<bool(bool)> func;
+        bool restore_on_setup_{false};
 };
 
 }
